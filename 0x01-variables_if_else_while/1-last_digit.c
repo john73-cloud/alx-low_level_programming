7c5f06dbe38ce7a4710ef6b07b7932f2d605ad85
John Uguru <johnuguru73@gmail.com>
2022-09-09 11:52:02 -0700

    main

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,17 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ */
 int main(void)
 {
 	int n;
+	int m = n%10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int m = n%10;
 	if (m>5)
 	{
 		printf("Last digit of %d is %d and is greater than 5",n,m);
