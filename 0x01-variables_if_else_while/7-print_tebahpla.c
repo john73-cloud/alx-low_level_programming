80d6726f325a9921dd711d1d01a275647002c26a
John Uguru <johnuguru73@gmail.com>
2022-09-10 08:05:17 -0700

    main

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 ( Success)
+ */
+int main(void)
+{
+	int i;
+	int al[26] = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q',
+		'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(al[i]);
+	}
+	putchar('\n');
+	return (0);
+}
