0bf4fb287da76c3e3b5050d8c132b16019e756ac
John Uguru <johnuguru73@gmail.com>
2022-09-10 07:17:36 -0700

    main

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	int i;
-	char al[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-		'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char al[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n','o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	for (i = 0; i < 26; i++)
 	{
 		putchar(al[i]);
