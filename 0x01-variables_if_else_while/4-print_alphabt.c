17c2e03e0ab9e853654e79dbb66fa8211e94b782
John Uguru <johnuguru73@gmail.com>
2022-09-10 07:30:11 -0700

    main

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 ( Success)
+ */
+int main(void)
+{
+	int i;
+	char al[26] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	for (i = 0; i < 26; i++)
+	{
+		putchar(al[i]);
+	}
+	putchar('\n');
+	return (0);
+}
