95d3b347e11e75a72bbdb36067e647b898f92caf
John Uguru <johnuguru73@gmail.com>
2022-09-10 07:53:20 -0700

    main

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 ( Success)
+ */
+int main(void)
+{
+	int i;
+	char al[26] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(al[i]);
+	}
+	putchar('\n');
+	return (0);
+}
