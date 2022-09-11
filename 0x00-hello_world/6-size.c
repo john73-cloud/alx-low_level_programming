88a1180d892c14ee6b410af4dbeb850b62fcadeb
John Uguru <johnuguru73@gmail.com>
2022-09-11 07:34:58 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@ int main()
 	int intType;
 	long int lintType;
 	long long int lliType;
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of char: %zu byte(s)\n", sizeof(intType));
-	printf("Size of char: %zu byte(s)\n", sizeof(lintType));
-	printf("Size of char: %zu byte(s)\n", sizeof(lliType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
     return 0;
 }
