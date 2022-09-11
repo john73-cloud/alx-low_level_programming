e53e0e402969cc0c63131e6b6103de97ec6a5816
John Uguru <johnuguru73@gmail.com>
2022-09-11 08:06:20 -0700

    main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,20 @@
-
-
 #include<stdio.h>
-int main() 
+/**
+ * Description - 'main'
+ * return (0) - always a success
+ */
+int main(void)
 {
 	char charType;
 	int intType;
 	long int lintType;
 	long long int lliType;
 	float ftype;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(lintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
-	printf("Size of a float: %zu byte\n", sizeof(ftype));
-    return 0;
+	printf("Size of a float: %zu byte(s)\n", sizeof(ftype));
+	return (0);
 }
