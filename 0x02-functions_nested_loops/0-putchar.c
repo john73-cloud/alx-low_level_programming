b87bd544cdb7ea56917a718a09f63936a84092b7
John Uguru <johnuguru73@gmail.com>
2022-09-13 11:08:52 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -19,5 +19,5 @@ int main (void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
