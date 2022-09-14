d112d28d84c229a5e18655aa7be540bcbdbdc92d
John Uguru <johnuguru73@gmail.com>
2022-09-14 04:16:19 -0700

    main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include "1-main.h"
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
@@ -7,13 +8,6 @@
  */
 int main(void)
 {
-	int i;
-	char al[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	for (i = 0; i < 26; i++)
-	{
-		_putchar(al[i]);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
