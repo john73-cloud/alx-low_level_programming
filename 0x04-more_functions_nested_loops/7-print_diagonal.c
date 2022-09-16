a2754782a1a131382f6ac66daea612abe4be4481
John Uguru <johnuguru73@gmail.com>
2022-09-16 05:32:08 -0700

    main

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - check the code
+ *@n: integer
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int b = 0;
+
+		for (b = 0; b < n; b++)
+		{
+			putchar('\');
+		}
+		putchar('\n');
+}
