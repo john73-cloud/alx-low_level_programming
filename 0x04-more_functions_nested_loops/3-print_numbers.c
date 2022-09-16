e907409df303890572e5562355de62a5829aa5d4
John Uguru <johnuguru73@gmail.com>
2022-09-16 04:31:24 -0700

    main

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(void)
 	int i;
 	int c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-	for (i = 0; i <= 10; i++;)
+	for (i = 0; i <= 10; i++)
 	{
 		putchar(c[i]);
 	}
