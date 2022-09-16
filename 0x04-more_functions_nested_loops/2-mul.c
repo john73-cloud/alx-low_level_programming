1b2b6968210ac0a18c691e076cc31a0ffbac59c2
John Uguru <johnuguru73@gmail.com>
2022-09-16 04:13:52 -0700

    main

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul - check the code.
+ *@a: an integer
+ *@b: an integer
+ * Return: zero  0 or one 1.
+ */
+
+int mul(int a, int b)
+{
+int r;
+r = a * b;
+return (r);
+}
