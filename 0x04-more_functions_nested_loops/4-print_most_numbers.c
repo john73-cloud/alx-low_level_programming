06547666beab8614be4012b487bb9d666f824e68
John Uguru <johnuguru73@gmail.com>
2022-09-16 04:40:20 -0700

    @main@

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+	int c[] = {'0', '1', '3', '5', '6', '7', '8', '9'};
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(c[i]);
+	}
+putchar('\n');
+}
