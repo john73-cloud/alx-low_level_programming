a382aaff7bfcb4f5921449746d73cedbdf1757da
John Uguru <johnuguru73@gmail.com>
2022-09-16 04:47:20 -0700

    main

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	int c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	while (i < 10)
+	{
+	for (i = 0; i < 10; i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	i++;
+	}
+putchar('\n');
+}
