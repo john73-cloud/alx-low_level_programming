3c3e8803796fbf5377805b006a5a30a0ea6d7bd8
John Uguru <johnuguru73@gmail.com>
2022-09-16 04:28:13 -0700

    main

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+	int c[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+	for (i = 0; i <= 10; i++;)
+	{
+		putchar(i);
+	}
+putchar('\n');
+}
