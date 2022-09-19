cb720a548921291e7bbc5db5c3459e3f10f4da59
John Uguru <johnuguru73@gmail.com>
2022-09-19 12:30:08 -0700

    main

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _strlen - reset num to 98
+* @s: variable
+* Return: str length
+*/
+
+int _strlen(char *s)
+{
+	int r = strlen(s);
+
+	return (r);
+}
