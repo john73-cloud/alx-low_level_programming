99a809aacc768ad24d2f934c9cc783f78273aba6
John Uguru <johnuguru73@gmail.com>
2022-09-19 13:43:58 -0700

    main

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* rev_string - reset num to 98
+* @s: variable
+* Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int len = strlen(s);
+	int str[len];
+
+	for (i = len - 1; i > -1; i--)
+	{
+		strcat(str, s[i]);
+	}
+	s = str;
+}
+
