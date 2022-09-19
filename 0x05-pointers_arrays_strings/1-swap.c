75959e0facef129fbcd4cec94f7fc9926e1a1fc4
John Uguru <johnuguru73@gmail.com>
2022-09-19 11:47:56 -0700

    main

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* swap_int - reset num to 98
+* @b: variable
+* @a: variable
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *tmp = *a;
+	*a = *b;
+	*b = *tmp;
+}
