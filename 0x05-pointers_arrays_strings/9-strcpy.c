a81d86c23d73579e974635b140b1178705d2cf66
John Uguru <johnuguru73@gmail.com>
2022-09-19 21:50:42 -0700

    main

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _srcpy - reset num to 98
+* @dest: variable
+* @src: variable
+* Return: main
+*/
+
+char *_srcpy(char *dest, char *src)
+{
+	srcpy(dest, src);
+	return (dest);
+}
