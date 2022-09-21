0b7ce27bae3da706dfa0eb4709975673b93c5ff8
John Uguru <johnuguru73@gmail.com>
2022-09-21 01:27:08 -0700

    main

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _strcat - concating two strings
+ * @dest: destination
+ * @src: source
+ * Return: char *
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *x = strcat(dest, src);
+
+	return (x);
+}
