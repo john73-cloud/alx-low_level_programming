ef6e3d057b45fcedad25517e091a6bb98192c208
John Uguru <johnuguru73@gmail.com>
2022-09-26 00:31:12 -0700

    main

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strpbrk - string length
+ * @s: str
+ * @accept: str
+ * Return: char *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
