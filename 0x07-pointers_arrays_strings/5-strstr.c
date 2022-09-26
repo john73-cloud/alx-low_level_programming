55ca689aa9a63d9943eed71f45cdcc0bd0a682dd
John Uguru <johnuguru73@gmail.com>
2022-09-26 00:39:09 -0700

    main

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strstr - string length
+ * @haystack: str
+ * @needle: str
+ * Return: char *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
