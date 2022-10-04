93cad02c73e29928297701c276597c20772604af
John Uguru <johnuguru73@gmail.com>
2022-10-04 14:11:50 -0700

    main

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - array
+ * @size: size int
+ * @c: char
+ * Return: char /
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *str;
+
+	str = malloc(sizeof(c) * size);
+	if (size == 0)
+	{
+		return ("NULL");
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
