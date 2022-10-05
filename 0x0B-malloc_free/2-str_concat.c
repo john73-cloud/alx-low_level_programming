cc8352a5e371a96953bc08ae37477c6bcdcde291
John Uguru <johnuguru73@gmail.com>
2022-10-04 21:45:07 -0700

    main

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,19 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int i2;
 
-	str = malloc(sizeof(char *) * twolen);
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == "")
+	{
+		len1 = 0;
+	}
+	if (s2 == "")
+	{
+		len2 = 0;
+	}
+	str = malloc(sizeof(char *) * len1 + len2);
 
 	for (i = 0; i < len1; i++)
 	{
