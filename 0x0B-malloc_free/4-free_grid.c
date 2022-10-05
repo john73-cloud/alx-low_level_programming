668db390d793cd605b7914108bcd3132bd2fda94
John Uguru <johnuguru73@gmail.com>
2022-10-05 04:29:08 -0700

    main

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - creating a grid
+ * @height: width of said grid
+ * @grid: height of aforementioned grid
+ * Return: int **
+ */
+void free_grid(int **grid, int height)
+{
+	printf("%d", height);
+	free(grid);
+}
