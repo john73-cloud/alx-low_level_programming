e603f9f66ee91193b261443c96e49479806156a9
John Uguru <johnuguru73@gmail.com>
2022-10-07 10:50:17 +0100

    modified:   0x0C-more_malloc_free/101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * main - mul
  * @arg: length of args
  * @argv: arg values
  * Return: void
  */
-int main(int arg, char *argv[])
-{
-	int o;
-	int t;
+int charArrayToInt(char *arr) {
+	int i, value, r, flag;
 
-	if (arg != 3)
+	flag = 1;
+	i = value = 0;
+	for( i = 0 ; i<strlen(arr) ; ++i)
+	{
+        // if arr contain negative number
+		if( i==0 && arr[i]=='-' )
+		{
+			flag = -1;
+			continue;
+		}
+		r = arr[i] - '0';
+		value = value * 10 + r;
+    }
+	value = value * flag;
+	return value;        
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int i2;
+	int i3;
+	int len1 = strlen(argv[1]) - 1;
+	int len2 = strlen(argv[2])- 1;
+	char *one = argv[1];
+	char *two = argv[2];
+	int on;
+	int tw;
+	char al[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+	'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+	'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+	'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	'V', 'W', 'X', 'Y', 'Z'};
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	o = atoi(argv[1]);
-	t = atoi(argv[2]);
-	if (o == 0 || t == 0)
+	for (i = 0; i < 52; i++)
 	{
-		printf("Error\n");
-		exit(98);
+		for (i2 = 0; argv[2][i2] != '\0'; i2++)
+		{
+			if (al[i] == argv[2][i2])
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+		for (i3 = 0; argv[2][i3] != '\0'; i3++)
+		{
+			if (al[i] == argv[2][i3])
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
 	}
-	else
-	{
-		printf("%d\n", o * t);
-	}
+	on = charArrayToInt(one);
+	tw = charArrayToInt(two);
+	printf("%d\n", on * tw);
 	return (0);
 }
