a66ce674ff26cf0e3db01aede84f62d3a2027495
John Uguru <johnuguru73@gmail.com>
2022-10-11 01:30:14 -0700

    main

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,5 +7,17 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->owner == NULL)
+	{
+		d->owner == "nill";
+	}
+	if (d->name == NULL)
+	{
+		d->name = "nill";
+	}
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
