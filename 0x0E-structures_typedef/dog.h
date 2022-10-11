537b589b4c7f0db252c34097a411a6efb9f1b5ef
John Uguru <johnuguru73@gmail.com>
2022-10-11 00:45:00 -0700

    main

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct declaration
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
