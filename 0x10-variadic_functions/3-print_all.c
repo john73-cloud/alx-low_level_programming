256c2778c7154be602019d3b1008bcde002dd22b
John Uguru <johnuguru73@gmail.com>
2022-10-13 13:43:54 -0700

    main

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+#include <string.h>
+#include "variadic_functions.h"
+/**
+ * one - func
+ * @ptr: ptr
+ */
+void one(va_list ptr)
+{
+	printf("%d", va_arg(ptr, int));
+}
+/**
+ * two - func
+ * @ptr: ptr
+ */
+void two(va_list ptr)
+{
+	printf("%f", va_arg(ptr, double));
+}
+/**
+ * three - func
+ * @ptr: ptr
+ */
+void three(va_list ptr)
+{
+	putchar(va_arg(ptr, int));
+}
+/**
+ * four - func
+ * @ptr: ptr
+ */
+void four(va_list ptr)
+{
+	printf("%s", va_arg(ptr, char *));
+}
+/**
+ * print_all - printing
+ * @format: formats
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
+	int len = strlen(format);
+	int i = 0;
+	m_t g[] = {
+		{'i', &one},
+		{'c', &three},
+		{'s', &four},
+		{'f', &two}
+	};
+	int x;
+	va_list ptr;
 
+	va_start(ptr, format);
+	while (i < len)
+	{
+		x = 0;
+		while (x < len)
+		{
+			if (format[i] == g[x].h)
+			{
+				g[x].fun(ptr);
+				if (i != len - 1)
+				{
+					printf(", ");
+				}
+			}
+			x++;
+		}
+		i++;
+	}
+	putchar('\n');
+	va_end(ptr);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct m - m
+ * @h: char
+ * @fun: fun
+ */
+typedef struct m
+{
+	char h;
+	void (*fun)(va_list);
+} m_t;
 #endif
