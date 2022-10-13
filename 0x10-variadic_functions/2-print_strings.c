ad60c4f4504d57bcb6bcc6d91a82b48de1b841c1
John Uguru <johnuguru73@gmail.com>
2022-10-13 11:04:12 -0700

    main

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - description
+ * @separator: separate
+ * @n: int len
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%s", va_arg(ptr, char *));
+		}
+		else
+		{
+			printf("%s", va_arg(ptr, char *));
+			if (i != n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+	}
+	putchar('\n');
+	va_end(ptr);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_all(const char * const format, ...)
+{
+
+}
