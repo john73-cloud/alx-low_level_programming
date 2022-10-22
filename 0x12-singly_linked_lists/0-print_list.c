0953eb123ae08ee934b0358503aa920b020cf51d
John Uguru <johnuguru73@gmail.com>
2022-10-21 21:05:38 -0700

    main

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_lists - func main
+ * print_list - func main
  * @h: main var
  * Return: size_t
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+	int i = 1;
 	list_t *m = malloc(sizeof(list_t));
 
 	m->str = h->str;
 	m->len = h->len;
 	m->next = h->next;
-	while (m->next != NULL)
+	for (i = 0; m != NULL; i++)
 	{
-		i++;
+		if (m->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%d] (%s)\n", m->len, m->str);
+		}
 		m = m->next;
 	}
+
 	return (i);
 }
