ff3a038834749d2765316cf97997fc893acd1dd9
John Uguru <johnuguru73@gmail.com>
2022-10-25 12:21:50 -0700

    main

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - function
+ * @head: pointer to a pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while((*head) != NULL)
+	{
+		tmp = (*head);
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+	(*head) = NULL;
+}
