a5e91eccc0761c7087e720ee3dc4befd826f92e5
John Uguru <johnuguru73@gmail.com>
2022-11-01 20:09:09 -0700

    main

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * read_textfile - main
+ * @filename: filename
+ * @letters: amount of words
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t i;
+	ssize_t m = 0;
+	FILE *ptr;
+	char c;
+
+	ptr = fopen(filename, "r");
+	if (ptr == NULL || filename == NULL)
+	{
+		return (0);
+	}
+	if (letters > 7)
+	{
+	}
+	for (i = 0; c != EOF; i++)
+	{
+		c = fgetc(ptr);
+		if (c != EOF)
+		{
+			printf("%c", c);
+			m++;
+		}
+	}
+	fclose(ptr);
+	return (m);
+}
