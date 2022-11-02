1904aa585115cd743c5eedcca69528c1c68d572d
John Uguru <johnuguru73@gmail.com>
2022-11-01 21:00:49 -0700

    main

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,12 +2,17 @@
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>
-
+/**
+ * create_file - create
+ * @filename: name
+ * @text_content: text
+ * Return: int
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int ptr;
 
-	ptr = open(filename, O_RDWR|O_CREAT|O_EXCL, 600);
+	ptr = open(filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
 	write(ptr, text_content, strlen(text_content));
 	close(ptr);
 	return (1);
