5b6285a37950f8be0625c9bfbde6d3e8ed183093
John Uguru <johnuguru73@gmail.com>
2022-11-01 21:28:24 -0700

    main

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * append_text_to_file - append
  * @filename: name
@@ -10,9 +11,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *ptr;
 
 	ptr = fopen(filename, "a");
-	if (ptr == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
-		return  (0);
+		return  (-1);
+	}
+	if (text_content == NULL && ptr == NULL)
+	{
+		return (-1);
+	}
+	else if (text_content == NULL && ptr != NULL)
+	{
+		return (1);
 	}
 	else
 	{
